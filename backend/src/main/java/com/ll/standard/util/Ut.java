5762cc88e345d9f20ae4eef26d794ced6838deec
jhs512 <jangka512@gmail.com>
2025-01-31 16:41:55 +0900

    work

--- backend/src/main/java/com/ll/standard/util/Ut.java
+++ backend/src/main/java/com/ll/standard/util/Ut.java
@@ -137,6 +137,10 @@ public class Ut {
             // 파일 확장자 추출
             String extension = getExtensionFromResponse(response);
 
+            if (extension.equals("tmp")) {
+                extension = getExtensionByTika(tempFilePath);
+            }
+
             // 파일명 추출
             String filename = getFilenameFromUrl(url);
 
@@ -145,6 +149,12 @@ public class Ut {
             mv(tempFilePath, newFilePath);
 
             return newFilePath;
+        }
+
+        public static String getExtensionByTika(String filePath) {
+            String mineType = AppConfig.getTika().detect(filePath);
+
+            return MIME_TYPE_MAP.getOrDefault(mineType, "tmp");
         }
 
         @SneakyThrows
