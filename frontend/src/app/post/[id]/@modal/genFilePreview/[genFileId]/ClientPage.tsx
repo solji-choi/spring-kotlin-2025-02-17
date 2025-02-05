9d3123148832f5e853cf54c6c3801e4b60311432
jhs512 <jangka512@gmail.com>
2025-02-05 11:17:57 +0900

    work

--- frontend/src/app/post/[id]/@modal/genFilePreview/[genFileId]/ClientPage.tsx
+++ frontend/src/app/post/[id]/@modal/genFilePreview/[genFileId]/ClientPage.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 
 import { components } from "@/lib/backend/apiV1/schema";
+import { getFileSizeHr } from "@/lib/business/utils";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -13,6 +14,8 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+
+import { Download } from "lucide-react";
 
 export default function ClientPage({
   id,
@@ -33,11 +36,23 @@ export default function ClientPage({
       <DialogContent className="max-w-[100dvh]">
         <DialogHeader>
           <DialogTitle>파일 미리보기</DialogTitle>
-          <DialogDescription>{id}번 글의 파일(sample1.jpg)</DialogDescription>
+          <DialogDescription>
+            {id}번 글의 파일({genFile.originalFileName})
+          </DialogDescription>
         </DialogHeader>
         <div className="flex justify-center">
           <img src={genFile.publicUrl} alt="" />
         </div>
+        <Button variant="link" asChild className="justify-start">
+          <a href={genFile.downloadUrl} className="flex items-center gap-2">
+            <Download />
+
+            <span>
+              {genFile.originalFileName}({getFileSizeHr(genFile.fileSize)})
+              다운로드
+            </span>
+          </a>
+        </Button>
         <DialogFooter className="gap-2">
           <Button
             variant="outline"
