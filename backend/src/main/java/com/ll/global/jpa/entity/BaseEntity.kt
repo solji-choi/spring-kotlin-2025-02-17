b0751f1112a2c41ab1e32d8e5f881f3422d65ed2
solji <solji.choi89@gmail.com>
2025-02-18 14:31:39 +0900

    baseTime, baseEntity use kotlin

---
+++ backend/src/main/java/com/ll/global/jpa/entity/BaseEntity.kt
@@ -0,0 +1,17 @@
+package com.ll.global.jpa.entity;
+
+import com.ll.standard.util.Ut
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    var id: Long? = null
+
+    val modelName: String
+        get() =  Ut.str.lcfirst(this::class.simpleName)
+}
