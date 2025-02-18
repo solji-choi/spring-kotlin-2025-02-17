1984bc920dc4829a790e457b1f7e99d4c391293c
solji <solji.choi89@gmail.com>
2025-02-18 11:18:23 +0900

    post genfile entity use kotlin

---
+++ backend/src/main/java/com/ll/domain/post/genFile/entity/PostGenFile.kt
@@ -0,0 +1,32 @@
+package com.ll.domain.post.genFile.entity;
+
+import com.ll.domain.base.genFile.genFile.entity.GenFile
+import com.ll.domain.post.post.entity.Post
+import jakarta.persistence.*
+
+@Entity
+class PostGenFile : GenFile {
+    enum class TypeCode {
+        attachment,
+        thumbnail
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    lateinit var post: Post
+
+    @Enumerated(EnumType.STRING)
+    lateinit var typeCode: TypeCode
+
+    constructor(post: Post, typeCode: TypeCode, fileNo: Int) : super(fileNo) {
+        this.post = post;
+        this.typeCode = typeCode;
+    }
+
+    override fun getOwnerModelId(): Long {
+        return post.id
+    }
+
+    override fun getTypeCodeAsStr(): String {
+        return typeCode.name
+    }
+}
