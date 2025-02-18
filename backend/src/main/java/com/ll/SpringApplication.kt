94e59f1d9e81e2ea0d9cbd65e245a5c55b328316
solji <solji.choi89@gmail.com>
2025-02-18 09:30:07 +0900

    main use kotlin

---
+++ backend/src/main/java/com/ll/SpringApplication.kt
@@ -0,0 +1,13 @@
+package com.ll
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@SpringBootApplication
+@EnableJpaAuditing
+class SpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringApplication>(*args)
+}
