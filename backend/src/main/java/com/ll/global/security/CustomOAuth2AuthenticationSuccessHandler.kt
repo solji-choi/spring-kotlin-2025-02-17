f3683674a96aaf17c3a9008fce9856ee4f5e585c
solji <solji.choi89@gmail.com>
2025-02-19 14:23:24 +0900

    CustomOAuth2AuthenticationSuccessHandler use kotlin

---
+++ backend/src/main/java/com/ll/global/security/CustomOAuth2AuthenticationSuccessHandler.kt
@@ -0,0 +1,24 @@
+package com.ll.global.security;
+
+import com.ll.domain.member.member.service.MemberService
+import com.ll.global.rq.Rq
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
+import org.springframework.stereotype.Component
+
+@Component
+class CustomOAuth2AuthenticationSuccessHandler(
+    private val memberService: MemberService,
+    private val rq: Rq
+) : SavedRequestAwareAuthenticationSuccessHandler() {
+    override fun onAuthenticationSuccess(request: HttpServletRequest, response: HttpServletResponse, authentication: Authentication) {
+        val actor = memberService.findById(rq.actor!!.id).get()
+        rq.makeAuthCookies(actor)
+
+        val redirectUrl = request.getParameter("state")
+
+        response.sendRedirect(redirectUrl)
+    }
+}
