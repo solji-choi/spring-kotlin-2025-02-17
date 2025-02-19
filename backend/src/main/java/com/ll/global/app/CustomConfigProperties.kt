8759faee38dcd8fd3843878710527b93021c4eb2
solji <solji.choi89@gmail.com>
2025-02-19 15:07:58 +0900

    CustomConfigProperties use kotlin

---
+++ backend/src/main/java/com/ll/global/app/CustomConfigProperties.kt
@@ -0,0 +1,17 @@
+package com.ll.global.app;
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+import java.util.List
+
+@Configuration
+@ConfigurationProperties(prefix = "custom")
+class CustomConfigProperties(
+    val notProdMembers: List<NotProdMember>
+) {
+    data class NotProdMember(val username: String, val nickname: String, val profileImgUrl: String) {
+        fun apiKey(): String {
+            return username
+        }
+    }
+}
