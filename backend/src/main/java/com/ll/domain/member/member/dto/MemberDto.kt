9ef270a8b92cbe7b78c3d4ae327a846d3c152f6c
solji <solji.choi89@gmail.com>
2025-02-19 10:41:57 +0900

    member dto use kotlin

---
+++ backend/src/main/java/com/ll/domain/member/member/dto/MemberDto.kt
@@ -0,0 +1,21 @@
+package com.ll.domain.member.member.dto;
+
+import com.ll.domain.member.member.entity.Member
+import java.time.LocalDateTime
+
+
+open class MemberDto(
+    val id: Long,
+    val createDate: LocalDateTime,
+    val modifyDate: LocalDateTime,
+    val nickname: String,
+    val profileImgUrl: String
+) {
+    constructor(member: Member) : this(
+        id = member.id!!,
+        createDate = member.createDate,
+        modifyDate = member.modifyDate,
+        nickname = member.nickname,
+        profileImgUrl = member.profileImgUrlOrDefault
+    )
+}
---
+++ backend/src/main/java/com/ll/domain/member/member/dto/MemberWithUsernameDto.kt
@@ -0,0 +1,10 @@
+package com.ll.domain.member.member.dto;
+
+import com.ll.domain.member.member.entity.Member
+import org.springframework.lang.NonNull
+
+
+class MemberWithUsernameDto(member: Member) : MemberDto(member) {
+    @NonNull
+    val username: String = member.username
+}
