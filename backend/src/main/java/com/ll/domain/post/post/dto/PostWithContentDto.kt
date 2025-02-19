1522c12a4c434f98e119c1a13977a302c3ce67ee
solji <solji.choi89@gmail.com>
2025-02-19 10:32:18 +0900

    postWithContent dto use kotlin

---
+++ backend/src/main/java/com/ll/domain/post/post/dto/PostWithContentDto.kt
@@ -0,0 +1,15 @@
+package com.ll.domain.post.post.dto;
+
+import com.ll.domain.post.post.entity.Post
+
+class PostWithContentDto(
+    val content: String,
+    var actorCanModify: Boolean? = null,
+    var actorCanDelete: Boolean? = null,
+    private val post: Post
+) : PostDto(post) {
+    constructor(post: Post) : this(
+        content = post.content,
+        post = post
+    )
+}
