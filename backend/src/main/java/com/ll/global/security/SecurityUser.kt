c73e57a04261523f3d694311c6605efd773294e5
solji <solji.choi89@gmail.com>
2025-02-19 10:24:49 +0900

    SecurityUser use kotlin

---
+++ backend/src/main/java/com/ll/global/security/SecurityUser.kt
@@ -0,0 +1,18 @@
+package com.ll.global.security;
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.oauth2.core.user.OAuth2User
+
+class SecurityUser(
+    val id: Long,
+    username: String,
+    password: String,
+    val nickname: String,
+    authorities: Collection<out GrantedAuthority>
+) : User(username, password, authorities), OAuth2User {
+
+    override fun getAttributes(): Map<String, Any> = emptyMap()
+
+    override fun getName(): String = username
+}
