68c99d5b3cfe2b8c306b5347c4bea903039cd237
solji <solji.choi89@gmail.com>
2025-02-19 14:20:33 +0900

    CustomOAuth2UserService use kotlin

---
+++ backend/src/main/java/com/ll/global/security/CustomOAuth2UserService.kt
@@ -0,0 +1,45 @@
+package com.ll.global.security;
+
+import com.ll.domain.member.member.service.MemberService
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
+import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+class CustomOAuth2UserService(
+    private val memberService: MemberService
+) : DefaultOAuth2UserService() {
+
+    // 소셜 로그인이 성공할 때마다 이 함수가 실행된다.
+    @Transactional
+    override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
+        val oAuth2User = super.loadUser(userRequest);
+
+        val oauthId = oAuth2User.name
+        val providerTypeCode = userRequest
+                .clientRegistration
+                .registrationId
+                .toUpperCase(Locale.getDefault())
+
+        val attributes = oAuth2User.attributes
+        val attributesProperties = attributes["properties"] as Map<String, String>
+
+        val nickname = attributesProperties["nickname"]!!
+        val profileImgUrl = attributesProperties["profile_image"]!!
+        val username = "${providerTypeCode}__$oauthId"
+
+        val member = memberService
+            .modifyOrJoin(username, nickname, profileImgUrl)
+
+        return SecurityUser(
+                member.id,
+                member.username,
+                "",
+                member.nickname,
+                member.authorities
+        );
+    }
+}
